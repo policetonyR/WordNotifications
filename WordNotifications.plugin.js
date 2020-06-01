abd8adfe73de0cb61f4b0955492c74d895297bbc
Tony <31581859+policetonyR@users.noreply.github.com>
2020-06-01 00:49:38 -0400

    Added Userblacklist feature

--- WordNotifications.plugin.js
+++ WordNotifications.plugin.js
@@ -1,10 +1,6 @@
 //META{"name":"WordNotifications","website":"https://github.com/policetonyR/WordNotifications","source":"https://raw.githubusercontent.com/policetonyR/WordNotifications/master/WordNotifications.plugin.js"}*//
 
-// !!! Hey there! If you didn't come here from the BetterDiscord Discord server ( https://discord.gg/2HScm8j )  !!! //
-// !!! then please do not use whatever you were using that led you here, getting plugins from places other than !!! //
-// !!! the #plugin repo channel in the BD server can be dangerous, as they can be malicious and do bad things.  !!! //
-
-// testing update auto
+// ** Version edited by tony#9999 // t1dtony // 314488397467222017
 
 class WordNotifications {
     getName() {
@@ -14,7 +10,7 @@ class WordNotifications {
         return "Get notifications when certain words are said. Edited by tony#9999 to allow for muted servers to register.";
     }
     getVersion() {
-        return "0.0.6";
+        return "0.0.7";
     }
     getAuthor() {
         return "Qwerasd (edited by tony)";
@@ -33,6 +29,7 @@ class WordNotifications {
         this.cancelPatch = BdApi.monkeyPatch(BdApi.findModuleByProps("dispatch"), 'dispatch', { after: this.dispatch.bind(this) });
         this.words = BdApi.loadData('WordNotifications', 'words') || [];
         this.blacklist = BdApi.loadData('WordNotifications', 'blacklist') || [];
+		this.userbl = BdApi.loadData('WordNotifications', 'userbl') || [];
     }
     stop() {
         this.cancelPatch();
@@ -48,6 +45,8 @@ class WordNotifications {
         const message = data.methodArguments[0].message;
         if (this.blacklist.includes(message.guild_id))
             return;
+		if (this.userbl.includes(message.author.id))
+			return;
         if (this.currentChannel() === message.channel_id && require('electron').remote.getCurrentWindow().isFocused())
             return;
         //if (this.isMuted(message.guild_id, message.channel_id))
@@ -87,6 +86,8 @@ class WordNotifications {
         const words = document.createElement('textarea');
         const ignoreT = document.createElement('h6');
         const ignore = document.createElement('textarea');
+		const userignoreT = document.createElement('h6');
+		const userignore = document.createElement('textarea');
         const br = document.createElement('br');
         const button = document.createElement('button');
         button.innerText = 'Apply';
@@ -110,17 +111,32 @@ class WordNotifications {
         ignore.style.minHeight = '6ch';
         ignore.style.color = 'black';
         ignore.style.backgroundColor = 'white';
+		// ** added code
+		userignoreT.innerText = 'Ignored Users';
+        userignoreT.style.marginTop = '0.5ch';
+        userignoreT.style.marginBottom = '0.25ch';
+        userignore.placeholder = '(Optional) List of user IDs to ignore (e.g. "86004744966914048, 280806472928198656")';
+        userignore.value = this.userbl.join(', ');
+        userignore.style.width = '100%';
+        userignore.style.minHeight = '6ch';
+        userignore.style.color = 'black';
+        userignore.style.backgroundColor = 'white';
+		
         button.addEventListener('click', _ => {
             this.words = words.value.split(',').map(e => e.trim());
             BdApi.saveData('WordNotifications', 'words', this.words);
             this.blacklist = ignore.value.split(',').map(e => e.trim());
             BdApi.saveData('WordNotifications', 'blacklist', this.blacklist);
+            this.userbl = userignore.value.split(',').map(e => e.trim());
+            BdApi.saveData('WordNotifications', 'userbl', this.userbl);			
             document.getElementById('plugin-settings-Word Notifications').previousSibling.click();
         });
         div.appendChild(wordsT);
         div.appendChild(words);
         div.appendChild(ignoreT);
         div.appendChild(ignore);
+		div.appendChild(userignoreT);
+		div.appendChild(userignore);
         div.appendChild(br);
         div.appendChild(button);
         return div;
